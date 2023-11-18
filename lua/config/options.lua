390391a9d49a6a18d7b4246a70ff35c91c462ad0
Antonin Bonte <antonin.bonte@quickfds.eu>
2023-11-18 22:57:22 +0100

    Added format options to match Intellij

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- match idea
+opt.fileencoding = "utf-8"
+opt.fileformat = "unix"
+opt.shiftwidth = 4
+opt.tabstop = 4
