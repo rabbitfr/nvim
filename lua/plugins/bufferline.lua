4633f489ac4d2f5c361f989bbac61c758627d2c5
Antonin Bonte <antonin.bonte@quickfds.eu>
2023-11-19 16:40:07 +0100

    Updated separator style to slope

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      -- indicator = {
+      --   icon = "▎ ",
+      --   style = "icon",
+      -- },
+      separator_style = "slope",
+    },
+  },
+}
