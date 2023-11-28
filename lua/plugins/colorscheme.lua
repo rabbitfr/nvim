8df4bfb669b61326f2975421930f1e7f0af28fb2
rabbit <rabbit@no-reply.com>
2023-11-29 00:17:42 +0100

    Should fix transparency with gruvbox-material theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,49 @@
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = "medium"
+-- vim.g.gruvbox_material_better_performance = 1
+-- vim.g.gruvbox_material_background = "medium"
+-- vim.g.gruvbox_material_transparent_background = 1
+
+-- vim.g.transparent_background = true
 
 return {
   -- add gruvbox
-  { "sainnhe/gruvbox-material" },
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   opts = {
+  --
+  --     better_performance = 1,
+  --     transparent_background = 2,
+  --   },
+  -- },
+  --
+  {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_better_performance = 1
+      -- Fonts
+      vim.g.gruvbox_material_disable_italic_comment = 1
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.g.gruvbox_material_enable_bold = 0
+      vim.g.gruvbox_material_transparent_background = 1
+      -- Themes
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
+
+      vim.cmd.colorscheme("gruvbox-material") -- For highlights customizations go to lua/core/highlights
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox-material",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox-material",
+  --   },
+  -- },
 }
 
 -- return {
